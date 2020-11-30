9577a16c13efebb6a752768317782a91a00cafa0
stefan-hennings <stefan.hennings4@gmail.com>
2020-11-30 08:23:06 +0100

    Switch approach implemented

--- src/FizzBuzzGeneral.java
+++ src/FizzBuzzGeneral.java
@@ -3,7 +3,14 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class FizzBuzzGeneral {
-    public static void main(String[] args) {
+    String firstWord;
+    int firstWordFrequency;
+    String secondWord;
+    int secondWordFrequency;
+    int printFrom;
+    int printUntil;
+
+    public FizzBuzzGeneral() {
         Properties properties = new Properties();
         try {
             properties.load(new FileInputStream("src/settings.properties"));
@@ -11,25 +18,52 @@ public class FizzBuzzGeneral {
             e.printStackTrace();
         }
 
-        String firstWord = properties.getProperty("firstWord");
-        int firstWordFrequency = Integer.parseInt(properties.getProperty("firstWordFrequency"));
 
-        String secondWord = properties.getProperty("secondWord");
-        int secondWordFrequency = Integer.parseInt(properties.getProperty("secondWordFrequency"));
+        firstWord = properties.getProperty("firstWord");
+        firstWordFrequency = Integer.parseInt(properties.getProperty("firstWordFrequency"));
 
-        int printFrom = Integer.parseInt(properties.getProperty("printFrom"));
-        int printUntil = Integer.parseInt(properties.getProperty("printUntil"));
+        secondWord = properties.getProperty("secondWord");
+        secondWordFrequency = Integer.parseInt(properties.getProperty("secondWordFrequency"));
 
+        printFrom = Integer.parseInt(properties.getProperty("printFrom"));
+        printUntil = Integer.parseInt(properties.getProperty("printUntil"));
+
+//        for (int i = printFrom; i <= printUntil; i++) {
+//            if (i % (firstWordFrequency * secondWordFrequency) == 0) {
+//                System.out.println(firstWord + secondWord);
+//            } else if (i % secondWordFrequency == 0) {
+//                System.out.println(secondWord);
+//            } else if (i % firstWordFrequency == 0) {
+//                System.out.println(firstWord);
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
         for (int i = printFrom; i <= printUntil; i++) {
-            if (i % (firstWordFrequency * secondWordFrequency) == 0) {
-                System.out.println(firstWord + secondWord);
-            } else if (i % secondWordFrequency == 0) {
-                System.out.println(secondWord);
-            } else if (i % firstWordFrequency == 0) {
-                System.out.println(firstWord);
-            } else {
-                System.out.println(i);
-            }
+            System.out.println(
+                    switch (modulusToZero(i)) {
+                        case (15) -> firstWord + secondWord;
+                        case (5) -> secondWord;
+                        case (3) -> firstWord;
+                        default -> i;
+                    }
+            );
         }
+    }
+
+    private int modulusToZero(int i) {
+        if (i % (firstWordFrequency * secondWordFrequency) == 0) {
+            return firstWordFrequency * secondWordFrequency;
+        } else if (i % secondWordFrequency == 0) {
+            return secondWordFrequency;
+        } else if (i % firstWordFrequency == 0) {
+            return firstWordFrequency;
+        } else {
+            return i;
+        }
+    }
+
+    public static void main(String[] args) {
+        new FizzBuzzGeneral();
     }
 }
